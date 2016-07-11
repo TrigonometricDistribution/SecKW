6b3b6ec1313cb634a04e83bfcda41ba31766c300
lucianoserafim <lucianoserafimdesouza@gmail>
2016-07-11 12:42:31 -0300

    Ajuste na função quantil.

--- R/qseckw.R
+++ R/qseckw.R
@@ -10,23 +10,23 @@
 #' qseckw(p,1,1,1,1,TRUE,FALSE)
 #' qseckw(p,3,0.5,2,2,TRUE,FALSE)
 
-qseckw<-function(p,a,b,c,beta,lambda,lower = TRUE,log.p = FALSE){
+qseckw<-function(p,a,b,c,lambda,lower = TRUE,log.p = FALSE){
 
   if (log.p == TRUE) {
     if (lower == TRUE){
       y=asec(2-p)
-      log((-1/lambda)*(log(1-(1-(1-((3/pi)*y)^(1/beta))^(1/b))^(1/a)))^(1/c))
+      log((-1/lambda)*(log(1-(1-(1-((3/pi)*y))^(1/b))^(1/a)))^(1/c))
     }else{
       y=asec(p)
-      log((-1/lambda)*(log(1-(1-(1-((3/pi)*y)^(1/beta))^(1/b))^(1/a)))^(1/c))
+      log((-1/lambda)*(log(1-(1-(1-((3/pi)*y))^(1/b))^(1/a)))^(1/c))
     }
   } else {
     if (lower == TRUE){
       y=asec(2-p)
-      (-1/lambda)*(log(1-(1-(1-((3/pi)*y)^(1/beta))^(1/b))^(1/a)))^(1/c)
+      (-1/lambda)*(log(1-(1-(1-((3/pi)*y))^(1/b))^(1/a)))^(1/c)
     }else{
       y=asec(p)
-      (-1/lambda)*(log(1-(1-(1-((3/pi)*y)^(1/beta))^(1/b))^(1/a)))^(1/c)
+      (-1/lambda)*(log(1-(1-(1-((3/pi)*y))^(1/b))^(1/a)))^(1/c)
     }
   }
 
